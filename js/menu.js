c260197336b0faecf488c07a28418b6b91b04ebd
Yafet-Aklilu <yafetaklilu420@gmail.com>
2024-10-10 12:53:20 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,1 @@
-function toggleMenu() {
-    const menu = document.querySelector('.oi oi-menu');
-    menu.classList.toggle('open'); 
-    if (menu.classList.contains('open')) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-  }
-  
+
